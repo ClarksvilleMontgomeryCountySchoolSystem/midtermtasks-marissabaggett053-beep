140607e8758a298197937ac481b98130348b0fbb
Karen Handy <karen.handy@cmcss.net>
2025-12-19 09:35:57 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -40,10 +40,10 @@ if not TESTING:
     item, price, quantity = get_purchase_info()
 
 # TODO: Calculate subtotal, tax, and total
-subtotal = float(price * quantity)
+subtotal = price * quantity
 # Tax rate: 9.5%
 tax = subtotal * 0.095
-total = subtotal + tax_amount
+total = subtotal + tax
 
 # TODO: Round total to 2 decimal places using round()
 total = round(total,2)
